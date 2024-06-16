5b14cf2636af6b8834adaf831523ba1c9da00640
pramendraindian <pramendra.indian@gmail.com>
2024-06-16 16:52:39 +0530

    added JWT Authentication and Authorization

---
+++ 02-Angular-DOTNET-CORE-JWT/AngularDOTNETCoreJWT/AngularDOTNETCoreJWT.Server/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using AngularDOTNETCoreJWT.Server.Policies;
+
+namespace AngularDOTNETCoreJWT.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        [Route("GetUserData")]
+        [Authorize(Policy = Policies.User)]
+        public IActionResult GetUserData()
+        {
+            return Ok("I am a customer of this site!!");
+        }
+
+        [HttpGet]
+        [Route("GetAdminData")]
+        [Authorize(Policy = Policies.Admin)]
+        public IActionResult GetAdminData()
+        {
+            return Ok("Heyyyy !! Admin here...");
+        }
+    }
+}
---
+++ 02-Angular-DOTNET-CORE-JWT/AngularDOTNETCoreJWT/AngularDOTNETCoreJWT.Server/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using AngularDOTNETCoreJWT.Server.Policies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+           .AddJwtBearer(options =>
+           {
+               options.RequireHttpsMetadata = false;
+               options.SaveToken = true;
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidateIssuerSigningKey = true,
+                   ValidIssuer =builder.Configuration["Jwt:Issuer"],
+                   ValidAudience = builder.Configuration["Jwt:Audience"],
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"])),
+                   ClockSkew = TimeSpan.Zero // Override the default clock skew of 5 mins
+               };
+               builder.Services.AddCors();
+           });
+
+builder.Services.AddAuthorization(config =>
+{
+    config.AddPolicy(Policies.Admin, Policies.AdminPolicy());
+    config.AddPolicy(Policies.User, Policies.UserPolicy());
+});
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
