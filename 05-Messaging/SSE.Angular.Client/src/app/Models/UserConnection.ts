814e1d4f9e3fbe86906499e82e62d4c2c0de3979
pramendraindian <pramendra.indian@gmail.com>
2024-11-02 22:41:01 +0530

    Added joined group

---
+++ 05-Messaging/SSE.Angular.Client/src/app/Models/UserConnection.ts
@@ -0,0 +1,5 @@
+export interface UserConnection
+{
+    connectionId:string;
+    userId:string;
+}
--- 05-Messaging/SSE.Angular.Client/src/app/chat/chat.component.ts
+++ 05-Messaging/SSE.Angular.Client/src/app/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ChatService } from '../Services/chat.service';
+import { UserConnection } from '../Models/UserConnection';
 
 @Component({
   selector: 'app-chat',
@@ -11,6 +12,7 @@ export class ChatComponent {
   user = '';
   message = '';
   messages: string[] = [];
+  connections:UserConnection[]= [];
   constructor(private chatService: ChatService) {
     this.subscribeChatHubEvents();
   }
@@ -24,6 +26,7 @@ export class ChatComponent {
     this.chatService.hubConnection.on('GlobalMessageTopic', (connectionId,user, message,allConnectedIds) => {
       console.log(`Message From ConnectionId: ${connectionId}, User: ${user}, Message: ${message}`);
       console.log(allConnectedIds);
+      this.connections=allConnectedIds;
       if(this.connectionId===connectionId){
         this.messages.push(`You pinged: ${message}`);
       }else{
@@ -33,12 +36,15 @@ export class ChatComponent {
     this.chatService.hubConnection.on('onChatInit', (connectionId,allConnectedIds) => {
       console.log(`Connection Id: ${connectionId} joined the chat`);
       console.log(allConnectedIds);
+      this.connections=allConnectedIds;
       this.connectionId=connectionId;
       //alert(`Connection Id: ${connectionId}`);
+      this.chatService.broadcastMessage(this.user, "Hello There I have joined the chat group");
     });
     this.chatService.hubConnection.on('onChatDestroy', (connectionId,allConnectedIds) => {
       console.log(`Connection Id: ${connectionId} left the chat !!!`);
       console.log(allConnectedIds);
+      this.connections=allConnectedIds;
       this.messages.push(`ConnectionId [${connectionId}] left the chat !!`);
       //alert(`Connection Id: ${connectionId}`);
     });
